73498e59c1e5ab56882bb93b069ee14eba703387
Jerry Lee <jerry.lee.tw.1992@gmail.com>
2020-02-07 18:15:23 +0800

    Initial Commit

---
+++ ReplayKitBySwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReplayKitBySwiftUI
+//
+//  Created by Jerry LEE on 2020/2/7.
+//  Copyright © 2020 JerryStudio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
