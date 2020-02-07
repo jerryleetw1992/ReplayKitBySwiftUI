ad45ffd8178579570e00d34645356fc085ed3b39
Jerry Lee <jerry.lee.tw.1992@gmail.com>
2020-02-07 18:21:13 +0800

    Tap to change the color of the text

--- ReplayKitBySwiftUI/ContentView.swift
+++ ReplayKitBySwiftUI/ContentView.swift
@@ -9,8 +9,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isBool = false
+    
     var body: some View {
         Text("Hello, World!")
+            .foregroundColor(isBool ? .red : .green)
+            .onTapGesture {
+                self.isBool.toggle()
+            }
     }
 }
 
